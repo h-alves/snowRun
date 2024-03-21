0fb0579c5f433942647614c4eafe3cfe932456de
Henrique Alves <henriqueasemmer@gmail.com>
2024-03-21 16:09:51 -0300

    Initial commit

---
+++ snowTruck/View/GameScene.swift
@@ -0,0 +1,13 @@
+//
+//  GameScene.swift
+//  snowTruck
+//
+//  Created by Henrique Semmer on 21/03/24.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+}
---
+++ snowTruck/View/GameViewController.swift
@@ -0,0 +1,14 @@
+//
+//  GameViewController.swift
+//  snowTruck
+//
+//  Created by Henrique Semmer on 21/03/24.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+}
